5494695ff6c16108f4408940da5dbb4735ae4fd9
JaykrushnDhameliya <jkdhameliya276@gmail.com>
2023-09-15 21:52:59 +0530

    add noopener noreferrer

--- src/components/Catalouge/index.jsx
+++ src/components/Catalouge/index.jsx
@@ -10,12 +10,12 @@ const Catalouge = () => {
   }, []);
 
   const Ceramic=[
-    {name:'300x600mm (Matt Finish)',src:'/assets/Ceramic/1.jpg',pdf:"https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/MATT.pdf?alt=media&token=d04e344d-de00-456f-b17b-5195a7745442",pdfName:'MATT.pdf'},
-    {name:'300x600mm (Glossy Finish)',src:'/assets/Ceramic/2.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/GLOSSYFinish.pdf?alt=media&token=b3c00f2e-9329-4419-a6e7-13720eb2d380',pdfName:'GLOSSYFinish.pdf'},
-    {name:'300x600mm (Elevation Series)',src:'/assets/Ceramic/3.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/GLOSSYELEVATION.pdf?alt=media&token=f120a993-dd47-44ae-98ac-b114742d9ec1',pdfName:'GLOSSYELEVATION.pdf'},
-    {name:'300x600mm Digital Wall Tiles (Elevation Series)',src:'/assets/Ceramic/4.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/ELEVATION.pdf?alt=media&token=07da97ee-443b-4d98-9c56-d5700b905ce7',pdfName:'ELEVATION.pdf'},
-    {name:'300x600mm Digital Wall Tiles(Glossy Finish)',src:'/assets/Ceramic/5.jpg',pdf:'https://console.firebase.google.com/project/blaze-f3c9e/storage/blaze-f3c9e.appspot.com/files',pdfName:'DigitalGlossyFinish.pdf'},
-    {name:'300x600mm Digital Wall Tiles(Matt Elevation)',src:'/assets/Ceramic/6.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/MATTELEVATION.pdf?alt=media&token=d673446e-c6dc-4ae2-83e1-d2be44b4d1b5',pdfName:'MATTELEVATION.pdf'},
+    {name:'300x600mm (Matt Finish)',src:'/assets/Ceramic/1.jpg',pdf:"https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/MATT.pdf?alt=media&token=3c3414d8-acab-4125-b5ca-d68b88d217e1",pdfName:'MATT.pdf'},
+    {name:'300x600mm (Glossy Finish)',src:'/assets/Ceramic/2.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/GLOSSYFinish.pdf?alt=media&token=c19c8ef4-6e20-4977-8448-6996790f2d26',pdfName:'GLOSSYFinish.pdf'},
+    {name:'300x600mm (Elevation Series)',src:'/assets/Ceramic/3.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/GLOSSYELEVATION.pdf?alt=media&token=4bca23ae-8e8b-4119-9a87-e2cdb05d826a',pdfName:'GLOSSYELEVATION.pdf'},
+    {name:'300x600mm Digital Wall Tiles (Elevation Series)',src:'/assets/Ceramic/4.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/ELEVATION.pdf?alt=media&token=a1fae88d-e600-4a31-a754-dc1c4ac55144',pdfName:'ELEVATION.pdf'},
+    {name:'300x600mm Digital Wall Tiles(Glossy Finish)',src:'/assets/Ceramic/5.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/DigitalGlossyFinish.pdf?alt=media&token=6d58d1ec-584d-4582-8212-21099aefc28c',pdfName:'DigitalGlossyFinish.pdf'},
+    {name:'300x600mm Digital Wall Tiles(Matt Elevation)',src:'/assets/Ceramic/6.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/MATTELEVATION.pdf?alt=media&token=a2a39e7e-41ee-4183-bda1-a208bf940c19',pdfName:'MATTELEVATION.pdf'},
   ]
   const Senaric=[
     {name:'300x600mm (GLOSSY(1001-1300))',src:'/assets/Sanitary/1.jpg',pdf:"https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/GLOSSY(1001-1300).pdf?alt=media&token=b4ca0d9b-1f65-4b8e-887a-8f134b2c2a1d",pdfName:'GLOSSY(1001-1300).pdf'},
@@ -26,24 +26,24 @@ const Catalouge = () => {
     {name:'300x600mm Digital Wall Tiles(Prism Collection)',src:'/assets/Sanitary/6.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/PrismCollection.pdf?alt=media&token=0a172167-9783-433e-8bc1-45ab68a2bdcb',pdfName:'PrismCollection.pdf'},
   ]
   const Agriculture=[
-    {name:'300x600mm FISH',src:'/assets/Agriculture/1.jpg',pdf:"https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/FISH.pdf?alt=media&token=bc852496-63e4-4a83-ac6e-abec2b843a26",pdfName:'FISH.pdf'},
-    {name:'300x600mm (GLELE)',src:'/assets/Agriculture/2.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/GLELE.pdf?alt=media&token=0c337c0a-8c6e-4239-bce0-159f4df507e0',pdfName:'GLELE.pdf'},
-    {name:'300x600mm (ELEGANTE)',src:'/assets/Agriculture/3.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/GLOSSYELEGANTE.pdf?alt=media&token=02f13530-659d-4ea6-b41e-60bab899e26d',pdfName:'GLOSSYELEGANTE.pdf'},
-    {name:'300x600mm Digital Wall Tiles (KITCHEN)',src:'/assets/Agriculture/4.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/KITCHEN.pdf?alt=media&token=459aabed-4d81-4753-9cb2-69f823d40db8',pdfName:'KITCHEN.pdf'},
-    {name:'300x600mm Digital Wall Tiles(MATT1)',src:'/assets/Agriculture/5.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/MATT1.pdf?alt=media&token=54151c08-3271-4e79-bda6-26e32fcdf6df',pdfName:'MATT1.pdf'},
-    {name:'300x600mm Digital Wall Tiles(MATTGVT)',src:'/assets/Agriculture/6.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/MATTGVT.pdf?alt=media&token=0cad61ee-a141-468e-b730-807884ce3e25',pdfName:'MATTGVT.pdf'},
+    {name:'300x600mm FISH',src:'/assets/Agriculture/1.jpg',pdf:"https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/FISH.pdf?alt=media&token=9ad42eca-d976-45d8-a7d8-c572ff8555ab",pdfName:'FISH.pdf'},
+    {name:'300x600mm (GLELE)',src:'/assets/Agriculture/2.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/GLELE.pdf?alt=media&token=60a4ecfb-a51c-4ddb-8526-8e92173eda94',pdfName:'GLELE.pdf'},
+    {name:'300x600mm (ELEGANTE)',src:'/assets/Agriculture/3.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/GLOSSYELEGANTE.pdf?alt=media&token=e2b04076-a7a0-407f-b292-5daffded8ab4',pdfName:'GLOSSYELEGANTE.pdf'},
+    {name:'300x600mm Digital Wall Tiles (KITCHEN)',src:'/assets/Agriculture/4.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/KITCHEN.pdf?alt=media&token=8368a792-3d51-4c46-b6f7-a9ec6fb5ca36',pdfName:'KITCHEN.pdf'},
+    {name:'300x600mm Digital Wall Tiles(MATT1)',src:'/assets/Agriculture/5.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/MATT1.pdf?alt=media&token=0e3a0b79-e224-4e1e-8772-b4740286347d',pdfName:'MATT1.pdf'},
+    {name:'300x600mm Digital Wall Tiles(MATTGVT)',src:'/assets/Agriculture/6.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/MATTGVT.pdf?alt=media&token=b7d60dde-4f18-4f89-ab36-a8093af6bce8',pdfName:'MATTGVT.pdf'},
   ]
   const Food=[
-    {name:'300x600mm CONCEPT',src:'/assets/Food/1.jpg',pdf:"https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/CONCEPT.pdf?alt=media&token=8ccc3124-5026-4298-ac90-12d45fbcca0d",pdfName:'CONCEPT.pdf'},
-    {name:'300x600mm (Digital Finish)',src:'/assets/Food/2.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/DigitalGlossyFinish.pdf?alt=media&token=b9f97098-b86a-4f7e-ae52-88e93235dbb5',pdfName:'DigitalGlossyFinish.pdf'},
-    {name:'300x600mm (GLCON)',src:'/assets/Food/3.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/GLCON.pdf?alt=media&token=7dbe108a-b6f5-48a2-8098-1cccf56f9860',pdfName:'GLCON.pdf'},
-    {name:'300x600mm Digital Wall Tiles (GLELE)',src:'/assets/Food/4.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/GLELE.pdf?alt=media&token=73591502-1f1c-422e-97eb-0fbaeb426554',pdfName:'GLELE.pdf'},
-    {name:'300x600mm Digital Wall Tiles(GLOSSY)',src:'/assets/Food/5.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/GLOSSY.pdf?alt=media&token=4dd9079b-4b46-4fec-9e61-08afdeb6903e',pdfName:'GLOSSY.pdf'},
-    {name:'300x600mm Digital Wall Tiles(GLOSSY MARVE SERIES)',src:'/assets/Food/6.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/blaze-f3c9e.appspot.com/o/GLOSSYMARVESERIES.pdf?alt=media&token=1520e8ef-48cd-4e5d-9025-810a2d05b995',pdfName:'GLOSSYMARVESERIES.pdf'},
+    {name:'300x600mm CONCEPT',src:'/assets/Food/1.jpg',pdf:"https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/CONCEPT.pdf?alt=media&token=eb74a533-a7eb-42d4-8c9a-d97998a552bf",pdfName:'CONCEPT.pdf'},
+    {name:'300x600mm (Digital Finish)',src:'/assets/Food/2.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/GLOSSYMARVESERIES.pdf?alt=media&token=355477d0-c4bd-43b0-9511-6ff952a9edfa',pdfName:'DigitalGlossyFinish.pdf'},
+    {name:'300x600mm (GLCON)',src:'/assets/Food/3.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/GLCON.pdf?alt=media&token=2ce1eb58-10e7-40dc-b3d6-54d812ba1dde',pdfName:'GLCON.pdf'},
+    {name:'300x600mm Digital Wall Tiles (GLELE)',src:'/assets/Food/4.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/GLELE.pdf?alt=media&token=4b0bbe44-64e9-409b-8947-878a778a2b3b',pdfName:'GLELE.pdf'},
+    {name:'300x600mm Digital Wall Tiles(GLOSSY)',src:'/assets/Food/5.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/GLOSSY.pdf?alt=media&token=b292bb9b-1b51-400b-8b97-87094287eb91',pdfName:'GLOSSY.pdf'},
+    {name:'300x600mm Digital Wall Tiles(GLOSSY MARVE SERIES)',src:'/assets/Food/6.jpg',pdf:'https://firebasestorage.googleapis.com/v0/b/jigu-10f4b.appspot.com/o/DigitalGlossyFinish.pdf?alt=media&token=5513fe52-663c-49db-a46e-1315d7d7c34b',pdfName:'GLOSSYMARVESERIES.pdf'},
   ]
   return (
     <>
-      {/* <section className="ttm-row portfolio-section clearfix ">
+      <section className="ttm-row portfolio-section clearfix ">
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
@@ -66,7 +66,7 @@ const Catalouge = () => {
                 </div>
                 <div className="featured-content">
                   <div className="post-header">
-                    <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener">
+                    <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener noreferrer">
                       <div className="ttm-box-post-date">
                         <span className="ttm-entry-date">
                           <time className="entry-date">
@@ -76,7 +76,7 @@ const Catalouge = () => {
                       </div>
                     </a>
                     <div className="post-meta">
-                      <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener">
+                      <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener noreferrer">
                         {" "}
                         <span className="ttm-meta-line comments-link">Download</span>
                       </a>
@@ -84,7 +84,7 @@ const Catalouge = () => {
                   </div>
                   <div className="post-title featured-title">
                     <h3>
-                      <a href={item.pdf} target="_blank" rel="noopener">{item.name}</a>
+                      <a href={item.pdf} target="_blank" rel="noopener noreferrer">{item.name}</a>
                     </h3>
                   </div>
                 </div>
@@ -119,7 +119,7 @@ const Catalouge = () => {
                 </div>
                 <div className="featured-content">
                   <div className="post-header">
-                    <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener">
+                    <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener noreferrer">
                       <div className="ttm-box-post-date">
                         <span className="ttm-entry-date">
                           <time className="entry-date">
@@ -129,7 +129,7 @@ const Catalouge = () => {
                       </div>
                     </a>
                     <div className="post-meta">
-                      <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener">
+                      <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener noreferrer">
                         {" "}
                         <span className="ttm-meta-line comments-link">Download</span>
                       </a>
@@ -137,7 +137,7 @@ const Catalouge = () => {
                   </div>
                   <div className="post-title featured-title">
                     <h3>
-                      <a href={item.pdf} target="_blank" rel="noopener">{item.name}</a>
+                      <a href={item.pdf} target="_blank" rel="noopener noreferrer">{item.name}</a>
                     </h3>
                   </div>
                 </div>
@@ -149,6 +149,7 @@ const Catalouge = () => {
           </div>
         </div>
       </section>
+      <button onclick={()=>download()}>Download!</button>
       <section className="ttm-row portfolio-section clearfix ">
         <div className="container">
           <div className="row">
@@ -171,7 +172,7 @@ const Catalouge = () => {
                 </div>
                 <div className="featured-content">
                   <div className="post-header">
-                    <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener">
+                    <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener noreferrer">
                       <div className="ttm-box-post-date">
                         <span className="ttm-entry-date">
                           <time className="entry-date">
@@ -181,7 +182,7 @@ const Catalouge = () => {
                       </div>
                     </a>
                     <div className="post-meta">
-                      <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener">
+                      <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener noreferrer">
                         {" "}
                         <span className="ttm-meta-line comments-link">Download</span>
                       </a>
@@ -189,7 +190,7 @@ const Catalouge = () => {
                   </div>
                   <div className="post-title featured-title">
                     <h3>
-                      <a href={item.pdf} target="_blank" rel="noopener">{item.name}</a>
+                      <a href={item.pdf} target="_blank" rel="noopener noreferrer">{item.name}</a>
                     </h3>
                   </div>
                 </div>
@@ -223,7 +224,7 @@ const Catalouge = () => {
                 </div>
                 <div className="featured-content">
                   <div className="post-header">
-                    <a download={item.pdfName} href={item.pdf} target="_blank" rel="noreferrer noopener">
+                    <a download={item.pdfName} href={item.pdf} target="_blank" rel="noreferrer noopener noreferrer">
                       <div className="ttm-box-post-date">
                         <span className="ttm-entry-date">
                           <time className="entry-date">
@@ -233,7 +234,7 @@ const Catalouge = () => {
                       </div>
                     </a>
                     <div className="post-meta">
-                      <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener">
+                      <a download={item.pdfName} href={item.pdf} target="_blank" rel="noopener noreferrer">
                         {" "}
                         <span className="ttm-meta-line comments-link">Download</span>
                       </a>
@@ -241,7 +242,7 @@ const Catalouge = () => {
                   </div>
                   <div className="post-title featured-title">
                     <h3>
-                      <a href={item.pdf} target="_blank" rel="noopener">{item.name}</a>
+                      <a href={item.pdf} target="_blank" rel="noopener noreferrer">{item.name}</a>
                     </h3>
                   </div>
                 </div>
@@ -252,7 +253,7 @@ const Catalouge = () => {
            })}
           </div>
         </div>
-      </section> */}
+      </section>
     </>
   );
 };
