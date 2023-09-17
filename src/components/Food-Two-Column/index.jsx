d5a81bc1050ea12b67154a915c9395d5ea4d875d
JaykrushnDhameliya <jkdhameliya276@gmail.com>
2023-09-17 18:44:44 +0530

    add collection chage food add beans

--- src/components/Food-Two-Column/index.jsx
+++ src/components/Food-Two-Column/index.jsx
@@ -9,36 +9,30 @@ export const FoodTwoColumn = () => {
       if (window.Isotope) initIsotope();
     }, 1000);
   }, []);
-  const FastFood = [
-    { img: "/assets/FastFood/veg aloo tikki.jpg", name: "veg aloo tikki" },
-    { img: "/assets/FastFood/aloo tikki burger.jpg", name: "aloo tikki burger" },
-    { img: "/assets/FastFood/Chips and burger.jpg", name: "Chips and burger" },
-    { img: "/assets/FastFood/Mix Fastfood.jpg", name: "Mix Fast food" },
-    { img: "/assets/FastFood/salad with burger.jpg", name: "salad with burger" },
+  const BeansItems = [
+    { img: "/assets/BeansItems/butter-beans-feature.jpeg", name: "butter beans- eature" },
+    { img: "/assets/BeansItems/dry-beans-stock.jpg", name: "dry beans" },
+    { img: "/assets/BeansItems/GettyImages.jpg", name: "Getty Beans" },
+    { img: "/assets/BeansItems/Kraft-Heinz-plays-with-bean.jpg", name: "Kraft Heinz" },
+    { img: "/assets/BeansItems/UK farmers.jpg", name: "UK farmers" },
   ];
-  const IndianFood = [
-    { img: "/assets/IndianFood/paneer naan with gravy.jpg", name: "paneer naan with gravy" },
-    { img: "/assets/IndianFood/samosa pizza roti.jpg", name: "samosa pizza roti" },
-    { img: "/assets/IndianFood/north-indian-food.jpg", name: "north-indian-food" },
-    { img: "/assets/IndianFood/dal fry.jpg", name: "dal fry" },
-    { img: "/assets/IndianFood/All Item.jpg", name: "All Item" },
+  const GreenItems = [
+    { img: "/assets/green/easy-green-bean-saute.jpg", name: "easy green bean" },
+    { img: "/assets/green/green-beans-with-bacon-square.jpg", name: "green beans with bacon square" },
+    { img: "/assets/green/GreenBeansPotatoes.jpg", name: "Green Beans Potatoes" },
+    { img: "/assets/green/maxresdefault.jpg", name: "maxres default" },
   ];
-  const Pizza = [
-    { img: "/assets/Pizza/Veggie Pizza.jpg", name: "Veggie Pizza" },
-    { img: "/assets/Pizza/pizza-cheese.jpg", name: "pizza-cheese" },
-    { img: "/assets/Pizza/Aussie Pizza.jpeg", name: "Aussie Pizza" },
-  ];
-  const southFood = [
-    { img: "/assets/southFood/idli with Dosa.jpg", name: "idli with Dosa" },
-    { img: "/assets/southFood/Masala Dosa.jpg", name: "Masala Dosa" },
-    { img: "/assets/southFood/south-indian-food.jpeg", name: "south-indian-food" },
+  const CoffeeItems = [
+    { img: "/assets/ghana/arabica-coffee-beans.jpg", name: "arabica coffee beans" },
+    { img: "/assets/ghana/Arabica-Coffee.jpg", name: "Arabica Coffee" },
+    { img: "/assets/ghana/Green Arabica beans.jpg", name: "Green Arabica beans" },
   ];
   return (
     <>
       <section className="works filter-img section-padding">
         <div className="container">
           <div className="row gallery">
-            <div className="col-lg-6 items mt-0 FastFood IndianFood Pizza southFood">
+            <div className="col-lg-6 items mt-0 Coffee Beans Green">
               <div className="section-head mb-0">
                 <h3>Food Items</h3>
 
@@ -47,17 +41,16 @@ export const FoodTwoColumn = () => {
                     <span data-filter="*" className="active">
                       All
                     </span>
-                    <span data-filter=".FastFood">Fast Food</span>
-                    <span data-filter=".IndianFood">Indian Food</span>
-                    <span data-filter=".Pizza">Pizza</span>
-                    <span data-filter=".southFood">south Food</span>
+                    <span data-filter=".Beans">Dry Beans</span>
+                    <span data-filter=".Green">Green Beans</span>
+                    <span data-filter=".Coffee">Coffee Beans</span>
                   </div>
                 </div>
               </div>
             </div>
-            {FastFood.map((item) => {
+            {BeansItems.map((item) => {
               return (
-                <div className="col-lg-6 items FastFood"  key={item.name}>
+                <div className="col-lg-6 items Beans"  key={item.name}>
                   <div className="item">
                     <div className="img">
                       <img
@@ -69,16 +62,16 @@ export const FoodTwoColumn = () => {
                       <h5>
                         <h5>{item.name}</h5>
                       </h5>
-                      <span>Fast Food</span>
+                      <span>Beans Items</span>
                       
                     </div>
                   </div>
                 </div>
               );
             })}
-            {IndianFood.map((item) => {
+            {GreenItems.map((item) => {
               return (
-                <div className="col-lg-6 items IndianFood"  key={item.name}>
+                <div className="col-lg-6 items Green"  key={item.name}>
                   <div className="item">
                     <div className="img">
                       <img
@@ -90,16 +83,16 @@ export const FoodTwoColumn = () => {
                       <h5>
                         <h5>{item.name}</h5>
                       </h5>
-                      <span>Indian Food</span>
+                      <span>Green Items</span>
                       
                     </div>
                   </div>
                 </div>
               );
             })}
-            {Pizza.map((item) => {
+            {CoffeeItems.map((item) => {
               return (
-                <div className="col-lg-6 items Pizza" key={item.name}>
+                <div className="col-lg-6 items Coffee" key={item.name}>
                   <div className="item">
                     <div className="img">
                       <img
@@ -111,28 +104,7 @@ export const FoodTwoColumn = () => {
                       <h5>
                         <h5>{item.name}</h5>
                       </h5>
-                      <span>Pizza</span>
-                      
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-            {southFood.map((item) => {
-              return (
-                <div className="col-lg-6 items southFood"  key={item.name}>
-                  <div className="item">
-                    <div className="img">
-                      <img
-                        src={item.img}
-                        alt=""
-                      />
-                    </div>
-                    <div className="cont vis">
-                      <h5>
-                        <h5>{item.name}</h5>
-                      </h5>
-                      <span>south Food</span>
+                      <span>Coffee Items</span>
                       
                     </div>
                   </div>
